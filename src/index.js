8a9420b35711416e1fd1ddf9b35d480797719d01
Duncan Leo <duncanleo97@gmail.com>
2018-06-15 02:02:57 +0800

    set overall font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import './index.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 window.MusicKitInstance = window.MusicKit.configure({
