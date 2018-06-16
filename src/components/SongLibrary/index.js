313017aa30e00c55a4af2548f3db93bd704798b2
Duncan Leo <duncanleo97@gmail.com>
2018-06-16 23:11:58 +0800

    minor overall styling fixes

--- src/components/SongLibrary/index.js
+++ src/components/SongLibrary/index.js
@@ -50,6 +50,7 @@ class SongLibrary extends Component {
   render() {
     return (
       <div className={styles.container}>
+        <span className={styles.title}>Songs</span>
         {
           this.state.songs.map((song) => (
             <Song key={song.id} song={song} onSelected={() => this.props.onSongSelected(song)} />
