bf8d49f2f1faaad83048486e52fb931175091f59
Duncan Leo <duncanleo97@gmail.com>
2018-06-16 17:08:23 +0800

    style PlayerControls

--- src/components/PlayerControls/index.js
+++ src/components/PlayerControls/index.js
@@ -2,35 +2,37 @@ import React, { Component } from 'react';
 
 import styles from './styles.scss';
 
+const padTimeUnit = (time) => (time < 10 ? `0${time.toFixed(0)}` : time.toFixed(0));
+
+const getFormattedTime = (ms) => {
+  let seconds = ms / 1000;
+  const hours = parseInt(seconds / 3600, 10);
+  seconds %= 3600;
+  const minutes = parseInt(seconds / 60, 10);
+  seconds %= 60;
+  return hours === 0 ? `${padTimeUnit(minutes)}:${padTimeUnit(seconds)}` : `${padTimeUnit(hours)}:${padTimeUnit(minutes)}`;
+};
+
 class PlayerControls extends Component {
   constructor(props) {
     super(props);
+
+    const { player } = window.MusicKitInstance;
+    const { PlaybackStates, Events } = window.MusicKit;
 
     this.state = {
       bitrate: 0,
       isPlaying: false,
       nowPlayingItem: null,
-      currentPlaybackTime: 0,
+      playbackMillis: 0,
+      playbackState: PlaybackStates.none,
     };
 
     this.togglePlay = this.togglePlay.bind(this);
     this.updateState = this.updateState.bind(this);
 
-    const { player } = window.MusicKitInstance;
-    const { Events } = window.MusicKit;
-
     player.addEventListener(Events.playbackStateDidChange, this.updateState);
     player.addEventListener(Events.playbackProgressDidChange, this.updateState);
-  }
-
-  updateState() {
-    const { player } = window.MusicKitInstance;
-    this.setState({
-      bitrate: player.bitrate,
-      currentPlaybackTime: player.currentPlaybackTime,
-      nowPlayingItem: player.nowPlayingItem,
-      isPlaying: player.isPlaying,
-    });
   }
 
   togglePlay() {
@@ -41,29 +43,34 @@ class PlayerControls extends Component {
     }
   }
 
-  render() {
+  updateState() {
     const { player } = window.MusicKitInstance;
+    this.setState({
+      bitrate: player.bitrate,
+      playbackMillis: player.currentPlaybackTime * 1000,
+      playbackState: player.playbackState,
+      nowPlayingItem: player.nowPlayingItem,
+      isPlaying: player.isPlaying,
+    });
+  }
+
+  render() {
+    const { PlaybackStates } = window.MusicKit;
+    const { player } = window.MusicKitInstance;
+    const progressValue = this.state.playbackState === PlaybackStates.playing ?
+      this.state.playbackMillis : null;
     const progressMax = this.state.nowPlayingItem !== null ?
       this.state.nowPlayingItem.attributes.durationInMillis : 0;
     return (
       <div className={styles.container}>
-        {
-          this.state.nowPlayingItem !== null ? (
-            <div>
-              <span>{this.state.nowPlayingItem.attributes.name}</span>
-              <span>{this.state.nowPlayingItem.attributes.artistName}</span>
-              <span>{this.state.nowPlayingItem.attributes.albumName}</span>
-            </div>
-          ) : null
-        }
+        <progress value={progressValue} max={progressMax} />
+        <span className={styles['current-time']}>{getFormattedTime(this.state.playbackMillis)}</span>
+        <span className={styles.duration}>{getFormattedTime(progressMax)}</span>
+        <span className={styles.bitrate}>{this.state.bitrate}kbps</span>
         <button className={styles['play-pause']} onClick={this.togglePlay}>
           { this.state.isPlaying ? 'Pause' : 'Play' }
         </button>
         <button className={styles.next} onClick={() => player.skipToNextItem()}>Next</button>
-        <progress value={this.state.currentPlaybackTime * 1000} max={progressMax} />
-        <span className={styles['current-time']}>{this.state.currentPlaybackTime}s</span>
-        <span className={styles.duration}>{progressMax / 1000}s</span>
-        <span className={styles.bitrate}>{this.state.bitrate}</span>
       </div>
     );
   }
