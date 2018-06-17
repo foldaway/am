1960a9bc502dc649197b6f9ce90582aba0b6ec87
Duncan Leo <duncanleo97@gmail.com>
2018-06-17 18:59:22 +0800

    scroll current playing item on queue change

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Song from '../Song';
@@ -8,39 +8,58 @@ import trackPropType from '../../prop_types/track';
 
 import styles from './styles.scss';
 
-const Player = ({ queue, nowPlayingItemIndex }) => (
-  <div className={styles.container}>
-    <div className={styles.queue}>
-      <span className={styles.title}>Queue</span>
-      {
-        queue.items.map((item, index) => (
-          <div
-            key={item.id}
-            className={nowPlayingItemIndex === index ? styles.active : null}
-          >
-            <Song
-              song={item}
-              onSelected={() => window.MusicKitInstance.player.changeToMediaAtIndex(index)}
-            />
-          </div>
-        ))
-      }
-    </div>
-    <div className={styles.controls}>
-      <PlayerControls
-        hasPrevious={queue.position > 0}
-        hasNext={queue.position < queue.length - 1}
-        onSeek={(time) => window.MusicKitInstance.player.seekToTime(time)}
-        onPrevious={() => window.MusicKitInstance.player.changeToMediaAtIndex(queue.position - 1)}
-        onNext={() => window.MusicKitInstance.player.skipToNextItem()}
-        onPlaybackChange={(isPaused) => (isPaused ?
-          window.MusicKitInstance.player.play() :
-          window.MusicKitInstance.player.pause()
-        )}
-      />
-    </div>
-  </div>
-);
+class Player extends Component {
+  constructor(props) {
+    super(props);
+
+    this.activeRefFunc = (e) => {
+      this.activeRef = e;
+    };
+  }
+
+  componentDidUpdate() {
+    this.activeRef.scrollIntoView({ behaviour: 'smooth' });
+  }
+
+  render() {
+    const { queue, nowPlayingItemIndex } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.queue}>
+          <span className={styles.title}>Queue</span>
+          {
+            queue.items.map((item, index) => (
+              <div
+                key={item.id}
+                className={nowPlayingItemIndex === index ? styles.active : null}
+                ref={nowPlayingItemIndex === index ? this.activeRefFunc : null}
+              >
+                <Song
+                  song={item}
+                  onSelected={() => window.MusicKitInstance.player.changeToMediaAtIndex(index)}
+                />
+              </div>
+            ))
+          }
+        </div>
+        <div className={styles.controls}>
+          <PlayerControls
+            hasPrevious={queue.position > 0}
+            hasNext={queue.position < queue.length - 1}
+            onSeek={(time) => window.MusicKitInstance.player.seekToTime(time)}
+            onPrevious={() => window.MusicKitInstance.player.changeToMediaAtIndex(queue.position - 1)}
+            onNext={() => window.MusicKitInstance.player.skipToNextItem()}
+            onPlaybackChange={(isPaused) => (isPaused ?
+              window.MusicKitInstance.player.play() :
+              window.MusicKitInstance.player.pause()
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 Player.defaultProps = {
   nowPlayingItemIndex: 0,
