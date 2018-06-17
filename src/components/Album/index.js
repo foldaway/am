3a808894e82af3414fbd92a00918d52ac1bc22cf
Duncan Leo <duncanleo97@gmail.com>
2018-06-17 12:16:18 +0800

    reduce AlbumLibrary art size

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -7,7 +7,7 @@ import albumPropType from '../../prop_types/album';
 
 const Album = (props) => (
   <div className={styles.container} onClick={() => props.onSelected(props.album)} role="presentation">
-    <img className={styles.art} src={props.album.attributes.artwork.url.replace('{w}', '400').replace('{h}', '400')} alt="" />
+    <img className={styles.art} src={props.album.attributes.artwork.url.replace('{w}', '300').replace('{h}', '300')} alt="" />
     <span className={styles.title}>{props.album.attributes.name}</span>
     <span className={styles.artist}>{props.album.attributes.artistName}</span>
   </div>
