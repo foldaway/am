8a1b1d6c9488734230c1394b867a3c6bbe063cb1
Duncan Leo <duncanleo97@gmail.com>
2018-06-18 22:25:02 +0800

    add basic styling for search

--- src/components/SearchCatalog/index.js
+++ src/components/SearchCatalog/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Album from '../Album';
 import Song from '../Song';
+import Loader from '../Loader';
 import styles from './styles.scss';
 
 class SearchCatalog extends Component {
@@ -11,11 +12,14 @@ class SearchCatalog extends Component {
 
     this.state = {
       term: '',
+      isSearching: false,
       songs: [],
       albums: [],
       artists: [],
+      playlists: [],
     };
 
+    this.getResultsView = this.getResultsView.bind(this);
     this.search = this.search.bind(this);
   }
 
@@ -27,42 +31,77 @@ class SearchCatalog extends Component {
     this.mounted = false;
   }
 
-  async search() {
-    const { songs, albums, artists } = await window.MusicKitInstance.api.search(this.state.term);
-    this.setState({
-      songs: songs.data,
-      albums: albums.data,
-      artists: artists.data,
-    });
-  }
-
-  render() {
+  getResultsView() {
+    if (this.state.isSearching) {
+      return (
+        <Loader />
+      );
+    }
     return (
-      <div className={styles.container}>
-        <input type="text" onChange={(e) => this.setState({ term: e.target.value })} />
-        <button onClick={this.search}>Search</button>
-        <div className={styles.section}>
-          <span className={styles.title}>Songs</span>
-          {
-            this.state.songs.map((song, index) => (
-              <Song song={song} onSelected={() => this.props.onSongSelected(this.state.songs, index)} />
-            ))
-          }
-        </div>
-        <div className={styles.section}>
-          <span className={styles.title}>Albums</span>
-          {
-            this.state.albums.map((album) => (
-              <Album album={album} onSelected={this.props.onAlbumSelected} />
-            ))
-          }
-        </div>
+      <div className={styles.results}>
         <div className={styles.section}>
           <span className={styles.title}>Artists</span>
           {
             this.state.artists.map((artist) => <span>{artist.attributes.name}</span>)
           }
         </div>
+        <div className={styles.section}>
+          <span className={styles.title}>Songs</span>
+          {
+            this.state.songs.map((song, index) => (
+              <Song
+                song={song}
+                onSelected={() => this.props.onSongSelected(this.state.songs, index)}
+              />
+            ))
+          }
+        </div>
+        <div className={styles.section}>
+          <span className={styles.title}>Albums</span>
+          <div className={styles.albums}>
+            {
+              this.state.albums.map((album) => (
+                <Album album={album} onSelected={this.props.onAlbumSelected} />
+              ))
+            }
+          </div>
+        </div>
+        <div className={styles.section}>
+          <span className={styles.title}>Playlists</span>
+          {
+            this.state.playlists.map((playlist) => <span>{playlist.attributes.name}</span>)
+          }
+        </div>
+      </div>
+    );
+  }
+
+  async search() {
+    this.setState({ isSearching: true });
+    const { songs, albums, artists, playlists } = await window.MusicKitInstance.api.search(this.state.term);
+    this.setState({
+      isSearching: false,
+      songs: songs.data,
+      albums: albums.data,
+      artists: artists.data,
+      playlists: playlists.data,
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <form
+          className={styles.search}
+          onSubmit={(e) => {
+          e.preventDefault();
+          this.search();
+        }}
+        >
+          <input type="text" onChange={(e) => this.setState({ term: e.target.value })} />
+          <input type="submit" value="Search" />
+        </form>
+        { this.getResultsView() }
       </div>
     );
   }
