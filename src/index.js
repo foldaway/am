92dbc7dcc51dbe7c209e0536a8168af76aa788eb
Duncan Leo <hello@duncanleo.me>
2018-12-22 11:51:41 +0800

    use high bitrate for playback

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import './index.scss';
 
 window.MusicKitInstance = window.MusicKit.configure({
+  bitrate: window.MusicKit.PlaybackBitrate.HIGH,
   developerToken: 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilc3RkFOTDlGNzUifQ.eyJpYXQiOjE1NDQ3NjAwNDAsImV4cCI6MTU1MjUzNjA0MCwiaXNzIjoiRVo0OTUzMjIzNiJ9.66rrfAZHJcehndvp_yHVbYg3UAy_pTyfmMFCyPhXBbC4SurfH7-5fEnAKvmVjtvcwicnEb1BDgNe8GO74niQtQ',
 });
 
