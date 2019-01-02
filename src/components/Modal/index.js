e14a71cc8aee55e840c7d54edf9ef1b21cd1ff02
Duncan Leo <hello@duncanleo.me>
2019-01-02 17:03:38 +0800

    use SCSS for modal styles

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './styles.scss';
-
-const backdropStyle = {
-  position: 'fixed',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  backgroundColor: 'rgba(0,0,0,0.3)',
-  padding: '8vw',
-};
-
-// The modal "window"
-const modalStyle = {
-  backgroundColor: '#fff',
-  borderRadius: 5,
-  minHeight: 300,
-  margin: '0 auto',
-  padding: 30,
-};
+import styles from './styles.scss';
 
 const Modal = ({ children, show, onClose }) => {
   if (!show) return null;
   return (
-    <div className="backdrop" style={backdropStyle}>
-      <div className="modal" style={modalStyle}>
+    <div className={styles.backdrop}>
+      <div className={styles.modal}>
         {children}
 
-        <div className="modal__footer">
-          <button className="modal__footer__close" onClick={onClose} type="button">
+        <div className={styles.modal__footer}>
+          <button className={styles.modal__footer__close} onClick={onClose} type="button">
             Close
           </button>
         </div>
