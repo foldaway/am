a697b852feea160c20cbb3666461457322e3cf28
Duncan Leo <hello@duncanleo.me>
2020-01-09 00:11:41 +0800

    chore: add commit ref

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import styles from './styles.scss';
 const Header = () => (
   <div className={styles.container}>
     <span>AM</span>
+    <span>{process.env.COMMIT_REF || 'dev'}</span>
   </div>
 );
 
