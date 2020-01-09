b0e42aa34cdf00a963dfe5ab8fdc998b21144da2
Duncan Leo <hello@duncanleo.me>
2020-01-09 22:51:03 +0800

    chore: make ArtistLibrary a functional component

--- src/components/ArtistLibrary/index.js
+++ src/components/ArtistLibrary/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
@@ -8,121 +8,100 @@ import Album from '../Album';
 
 /* eslint-disable no-await-in-loop */
 
-class ArtistLibrary extends Component {
-  constructor(props) {
-    super(props);
+function ArtistLibrary({ onAlbumSelected }) {
+  const [artists, setArtists] = useState([]);
+  const [currentArtist, setCurrentArtist] = useState(null);
+  const [indivAlbums, setIndivAlbums] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    this.state = {
-      artists: [],
-      currentArtist: null,
-      indivAlbums: [],
-      isLoading: false,
-    };
+  useEffect(() => {
+    async function load() {
+      let temp = [];
+      let results = [];
+      do {
+        results = await window.MusicKitInstance.api.library.artists(null, {
+          limit: 100,
+          offset: temp.length,
+        });
+        temp = temp.concat(results);
+      } while (results.length > 0);
 
-    this.load = this.load.bind(this);
-    this.loadIndiv = this.loadIndiv.bind(this);
-    this.setArtist = this.setArtist.bind(this);
-    this.getIndivView = this.getIndivView.bind(this);
-  }
+      setArtists(temp);
+    }
 
-  componentDidMount() {
-    this.mounted = true;
-    this.load();
-  }
+    load();
+  }, []);
 
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  setArtist(artist) {
-    this.setState({
-      isLoading: true,
-      currentArtist: artist,
-    }, () => {
-      this.loadIndiv();
-    });
-  }
-
-  async load() {
-    let temp = [];
-    do {
-      temp = await window.MusicKitInstance.api.library.artists(null, {
-        limit: 100,
-        offset: this.state.artists.length,
+  useEffect(() => {
+    if (currentArtist === null) {
+      return;
+    }
+    setIsLoading(true);
+    async function loadIndiv() {
+      const { id } = currentArtist;
+      const {
+        relationships,
+      } = await window.MusicKitInstance.api.library.artist(id, {
+        include: ['albums'],
       });
+      setIsLoading(false);
 
-      if (!this.mounted) {
-        break;
-      }
+      setIndivAlbums(relationships.albums.data);
+    }
 
-      this.setState({
-        artists: [...this.state.artists, ...temp],
-      });
-    } while (temp.length > 0);
-  }
+    loadIndiv();
+  }, [currentArtist]);
 
-  async loadIndiv() {
-    const { id } = this.state.currentArtist;
-    const { relationships } = await window.MusicKitInstance.api.library.artist(id, {
-      include: ['albums'],
-    });
-    this.setState({
-      isLoading: false,
-      indivAlbums: relationships.albums.data,
-    });
-  }
-
-  getIndivView() {
-    const { onAlbumSelected } = this.props;
-    const { currentArtist, indivAlbums } = this.state;
-
-    const { isLoading } = this.state;
+  function getIndivView() {
     if (isLoading) {
       return <Loader />;
     }
     return (
       <div className={styles.indiv}>
         <span className={styles.title}>
-          {
-            currentArtist !== null ? currentArtist.attributes.name : ''
-          }
+          {currentArtist !== null ? currentArtist.attributes.name : ''}
         </span>
         <span className={styles['album-count']}>
-          {
-            indivAlbums.length > 0 ? `${indivAlbums.length} albums` : null
-          }
+          {indivAlbums.length > 0 ? `${indivAlbums.length} albums` : null}
         </span>
         <div className={styles.albums}>
-          {
-            indivAlbums.length > 0 ? indivAlbums.map((album) => (
-              <Album key={album.id} album={album} onSelected={onAlbumSelected} />
-            )) : null
-          }
-          {
-            currentArtist !== null && indivAlbums.length === 0 ? <Loader /> : null
-          }
+          {indivAlbums.length > 0
+            ? indivAlbums.map((album) => (
+              <Album
+                key={album.id}
+                album={album}
+                onSelected={onAlbumSelected}
+              />
+            ))
+            : null}
+          {currentArtist !== null && indivAlbums.length === 0 ? (
+            <Loader />
+          ) : null}
         </div>
       </div>
     );
   }
 
-  render() {
-    const { artists } = this.state;
-    return (
-      <div className={styles.container}>
-        <div className={styles['artist-list']}>
-          {
-            artists.length > 0 ? artists.map((artist) => (
-              <div className={styles['artist-container']} onClick={() => this.setArtist(artist)} role="presentation">
-                <Artist artist={artist} />
-              </div>
-            )) : <Loader />
-          }
-        </div>
-        { this.getIndivView() }
+  return (
+    <div className={styles.container}>
+      <div className={styles['artist-list']}>
+        {artists.length > 0 ? (
+          artists.map((artist) => (
+            <div
+              className={styles['artist-container']}
+              onClick={() => setCurrentArtist(artist)}
+              role="presentation"
+            >
+              <Artist artist={artist} />
+            </div>
+          ))
+        ) : (
+          <Loader />
+        )}
       </div>
-    );
-  }
+      {getIndivView()}
+    </div>
+  );
 }
 
 ArtistLibrary.propTypes = {
