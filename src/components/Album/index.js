cc85bd1bd68fe8c4280c15b7a7fdaca3a6c25d7f
Duncan Leo <hello@duncanleo.me>
2020-01-09 21:55:56 +0800

    chore: migrate Album to styled-components

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -1,13 +1,88 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
 import AlbumSong from '../AlbumSong';
 import Loader from '../Loader';
 
-import styles from './styles.scss';
-
 import { imgURLGen, srcSetGen } from '../../util/img';
 import albumPropType from '../../prop_types/album';
+
+const Image = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: filter 30ms;
+  background: gray;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-areas:
+    "art"
+    "title"
+    "artist"
+    "songs";
+  grid-template-rows: auto auto auto 1fr;
+  font-size: 0.9em;
+  grid-row-gap: 3px;
+
+  &:hover {
+    cursor: pointer;
+
+    ${Image} {
+      filter: brightness(120%);
+    }
+  }
+`;
+
+const ArtWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  border-radius: 6px;
+  border: 1px solid $faintgray;
+  margin-bottom: 4px;
+`;
+
+const Spacer = styled.svg`
+  width: 100%;
+  height: auto;
+`;
+
+const Title = styled.span`
+  font-weight: 400;
+  color: ${(props) => props.theme.text.primary};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const Artist = styled.span`
+  font-weight: 400;
+  color: ${(props) => props.theme.text.secondary};
+  font-size: 0.9em;
+`;
+
+const SongList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 12px;
+
+  * {
+    margin: 2.5px 0;
+  }
+  :first-child {
+    margin-top: 0;
+  }
+  :last-child {
+    margin-bottom: 0;
+  }
+`;
 
 function Album({ album, onSelected }) {
   const [songs, setSongs] = useState([]);
@@ -33,23 +108,18 @@ function Album({ album, onSelected }) {
   const { url } = album.attributes.artwork || { url: '' };
 
   return (
-    <div
-      className={styles.container}
-      onClick={toggleSongList}
-      role="presentation"
-    >
-      <div className={styles['art-container']}>
-        <svg className={styles.spacer} viewBox="0 0 1 1" />
-        <img
-          className={styles.art}
+    <Wrapper onClick={toggleSongList} role="presentation">
+      <ArtWrapper>
+        <Spacer viewBox="0 0 1 1" />
+        <Image
           src={imgURLGen(url, { w: 75 })}
           srcSet={srcSetGen(url)}
           alt="album artwork"
         />
-      </div>
-      <span className={styles.title}>{album.attributes.name}</span>
-      <span className={styles.artist}>{album.attributes.artistName}</span>
-      <div className={styles.songs}>
+      </ArtWrapper>
+      <Title>{album.attributes.name}</Title>
+      <Artist>{album.attributes.artistName}</Artist>
+      <SongList>
         {songListVisible && songs.length > 0
           ? songs.map((song, index) => (
             <AlbumSong
@@ -64,8 +134,8 @@ function Album({ album, onSelected }) {
             <Loader />
           </div>
         ) : null}
-      </div>
-    </div>
+      </SongList>
+    </Wrapper>
   );
 }
 
