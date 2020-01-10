56509c2cf86241308c5b106fede013f233a1d95c
Duncan Leo <hello@duncanleo.me>
2020-01-11 01:25:32 +0800

    chore: make SongLibrary a functional component

--- src/components/SongLibrary/index.js
+++ src/components/SongLibrary/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Song from '../Song';
@@ -9,98 +9,76 @@ import styles from './styles.scss';
 
 const sleep = async (msec) => new Promise((resolve) => setTimeout(resolve, msec));
 
-class SongLibrary extends Component {
-  constructor(props) {
-    super(props);
+function SongLibrary({ onSongSelected }) {
+  const [songs, setSongs] = useState([]);
 
-    this.state = {
-      songs: [],
-    };
-
-    this.load = this.load.bind(this);
-    this.getSongElements = this.getSongElements.bind(this);
-  }
-
-  componentDidMount() {
-    this.mounted = true;
-    this.load();
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  getSongElements() {
+  function getSongElements() {
     const elements = [];
     let temp = '';
 
-    this.state.songs.forEach((song, index) => {
+    songs.forEach((song, index) => {
       const firstArtistNameLetter = song.attributes.artistName[0];
       if (temp !== firstArtistNameLetter) {
-        elements.push(<span key={firstArtistNameLetter} className={styles['group-title']}>{firstArtistNameLetter}</span>);
+        elements.push(
+          <span key={firstArtistNameLetter} className={styles['group-title']}>
+            {firstArtistNameLetter}
+          </span>,
+        );
       }
       temp = firstArtistNameLetter;
 
-      elements.push((
+      elements.push(
         <Song
           key={song.id}
           song={song}
-          onSelected={() => this.props.onSongSelected(this.state.songs, index)}
-        />
-      ));
+          onSelected={() => onSongSelected(songs, index)}
+        />,
+      );
     });
 
     return elements;
   }
+  useEffect(() => {
+    async function load() {
+      const sortFunc = (a, b) => {
+        const aa = a.attributes;
+        const ba = b.attributes;
 
-  async load() {
-    const sortFunc = (a, b) => {
-      const aa = a.attributes;
-      const ba = b.attributes;
+        const aan = aa.albumName || '';
+        const ban = ba.albumName || '';
 
-      const aan = aa.albumName || '';
-      const ban = ba.albumName || '';
+        return (
+          aa.artistName.localeCompare(ba.artistName)
+          || aan.localeCompare(ban)
+          || aa.trackNumber - ba.trackNumber
+          || aa.name.localeCompare(ba.name)
+        );
+      };
 
-      return aa.artistName.localeCompare(ba.artistName) ||
-        aan.localeCompare(ban) ||
-        aa.trackNumber - ba.trackNumber ||
-        aa.name.localeCompare(ba.name);
-    };
+      const results = [];
+      let temp = [];
+      do {
+        temp = await window.MusicKitInstance.api.library.songs(null, {
+          limit: 100,
+          offset: results.length,
+        });
 
-    const songs = [];
-    let temp = [];
-    do {
-      temp = await window.MusicKitInstance.api.library.songs(null, {
-        limit: 100,
-        offset: songs.length,
-      });
+        results.push(...temp);
 
-      if (!this.mounted) {
-        break;
-      }
+        await sleep(2);
+      } while (temp.length > 0);
 
-      songs.push(...temp);
-
-      await sleep(2);
-    } while (temp.length > 0);
-
-    songs.sort(sortFunc);
-
-    this.setState({
-      songs,
-    });
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <span className={styles.title}>Songs</span>
-        {
-          this.state.songs.length > 0 ? this.getSongElements() : <Loader />
-        }
-      </div>
-    );
-  }
+      songs.sort(sortFunc);
+      setSongs(results);
+    }
+    load();
+  }, []);
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Songs</span>
+      {songs.length > 0 ? getSongElements() : <Loader />}
+    </div>
+  );
 }
 
 SongLibrary.propTypes = {
