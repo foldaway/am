9667016662eddee43f694bffdbc8610a0c03c0ab
Duncan Leo <hello@duncanleo.me>
2020-01-11 01:09:50 +0800

    chore: make SideMenu a functional component

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,65 +1,71 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './styles.scss';
 
 /* eslint-disable no-await-in-loop */
 
-class SideMenu extends Component {
-  constructor(props) {
-    super(props);
+function SideMenu() {
+  const [playlists, setPlaylists] = useState([]);
+  useEffect(() => {
+    async function loadPlaylists() {
+      const results = [];
+      let temp = [];
+      do {
+        temp = await window.MusicKitInstance.api.library.playlists(null, {
+          limit: 100,
+          offset: temp.length,
+        });
 
-    this.state = {
-      playlists: [],
-    };
-
-    this.loadPlaylists = this.loadPlaylists.bind(this);
-    this.loadPlaylists();
-  }
-
-  async loadPlaylists() {
-    let temp = [];
-    do {
-      temp = await window.MusicKitInstance.api.library.playlists(null, {
-        limit: 100,
-        offset: this.state.playlists.length,
-      });
-
-      this.setState({
-        playlists: [...this.state.playlists, ...temp],
-      });
-    } while (temp.length > 0);
-  }
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.section}>
-          <span className={styles.title}>Library</span>
-          <NavLink activeClassName={styles.current} to="/library/recently-added">Recently Added</NavLink>
-          <NavLink activeClassName={styles.current} to="/library/artists">Artists</NavLink>
-          <NavLink activeClassName={styles.current} to="/library/albums">Albums</NavLink>
-          <NavLink activeClassName={styles.current} to="/library/songs">Songs</NavLink>
-        </div>
-        <div className={styles.section}>
-          <span className={styles.title}>Catalog</span>
-          <NavLink activeClassName={styles.current} to="/search">Search</NavLink>
-          <NavLink activeClassName={styles.current} to="/for-you">For You</NavLink>
-        </div>
-        <div className={[styles.section, styles.playlists].join(' ')}>
-          <span className={styles.title}>Playlists</span>
-          {
-            this.state.playlists.map((playlist) => (
-              <NavLink key={playlist.id} activeClassName={styles.current} to={`/library/playlist/${Buffer.from(playlist.id).toString('base64')}`} >
-                {playlist.attributes.name}
-              </NavLink>
-            ))
-          }
-        </div>
+        results.push(...temp);
+      } while (temp.length > 0);
+      setPlaylists(results);
+    }
+    loadPlaylists();
+  }, []);
+  return (
+    <div className={styles.container}>
+      <div className={styles.section}>
+        <span className={styles.title}>Library</span>
+        <NavLink activeClassName={styles.current} to="/library/recently-added">
+          Recently Added
+        </NavLink>
+        <NavLink activeClassName={styles.current} to="/library/artists">
+          Artists
+        </NavLink>
+        <NavLink activeClassName={styles.current} to="/library/albums">
+          Albums
+        </NavLink>
+        <NavLink activeClassName={styles.current} to="/library/songs">
+          Songs
+        </NavLink>
       </div>
-    );
-  }
+      <div className={styles.section}>
+        <span className={styles.title}>Catalog</span>
+        <NavLink activeClassName={styles.current} to="/search">
+          Search
+        </NavLink>
+        <NavLink activeClassName={styles.current} to="/for-you">
+          For You
+        </NavLink>
+      </div>
+      <div className={[styles.section, styles.playlists].join(' ')}>
+        <span className={styles.title}>Playlists</span>
+        {playlists.map((playlist) => (
+          <NavLink
+            key={playlist.id}
+            activeClassName={styles.current}
+            to={`/library/playlist/${Buffer.from(playlist.id).toString(
+              'base64',
+            )}`}
+          >
+            {playlist.attributes.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SideMenu;
