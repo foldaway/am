a7bbe74978fa6a8d83485307ee2f7494ccaf126a
Duncan Leo <hello@duncanleo.me>
2020-01-12 00:22:05 +0800

    refactor: make SearchCatalog a functional component

--- src/components/SearchCatalog/index.js
+++ src/components/SearchCatalog/index.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Link } from 'react-router-dom';
 import Autosuggest from 'react-autosuggest';
-import debounce from 'lodash.debounce';
-
+import { debounce, get } from 'lodash';
 import Album from '../Album';
 import Song from '../Song';
 import Artist from '../Artist';
@@ -11,227 +10,174 @@ import Loader from '../Loader';
 import styles from './styles.scss';
 import Playlist from '../Playlist';
 
-class SearchCatalog extends Component {
-  constructor(props) {
-    super(props);
+function SongsView({ songs, onSongSelected }) {
+  if (songs.length === 0) {
+    return null;
+  }
+  return (
+    <div className={styles.section}>
+      <span className={styles.title}>Songs</span>
+      {songs.map((song, index) => (
+        <Song
+          key={song.id}
+          song={song}
+          onSelected={() => onSongSelected(songs, index)}
+        />
+      ))}
+    </div>
+  );
+}
 
-    const query = new URLSearchParams(props.location.search);
-    const term = query.get('term');
-
-    this.state = {
-      term: term || '',
-      isSearching: term !== null,
-      songs: [],
-      albums: [],
-      artists: [],
-      playlists: [],
-      suggestions: [],
-      redirectTerm: null,
-    };
-
-    this.getResultsView = this.getResultsView.bind(this);
-    this.fetchSuggestions = debounce(this.fetchSuggestions.bind(this), 300);
-    this.clearSuggestions = this.clearSuggestions.bind(this);
-    this.search = this.search.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.onSuggestionSelected = this.onSuggestionSelected.bind(this);
-
-    this.getSongsView = this.getSongsView.bind(this);
-    this.getAlbumsView = this.getAlbumsView.bind(this);
-    this.getArtistsView = this.getArtistsView.bind(this);
-    this.getPlaylistsView = this.getPlaylistsView.bind(this);
-
-    this.getSuggestionView = (text) => (
-      <div className={styles.suggestion}>
-        <span>{text}</span>
+function AlbumsView({ albums, onAlbumSelected }) {
+  if (albums.length === 0) {
+    return null;
+  }
+  return (
+    <div className={styles.section}>
+      <span className={styles.title}>Albums</span>
+      <div className={styles.albums}>
+        {albums.map((album) => (
+          <Album key={album.id} album={album} onSelected={onAlbumSelected} />
+        ))}
       </div>
-    );
+    </div>
+  );
+}
 
-    if (term !== null) {
-      this.search();
-    }
+function PlaylistsView({ playlists }) {
+  if (playlists.length === 0) {
+    return null;
   }
-
-  componentDidMount() {
-    this.mounted = true;
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  onChange(event, { newValue }) {
-    this.setState({
-      term: newValue,
-    });
-  }
-
-  onSuggestionSelected(event, { suggestion }) {
-    this.setState({ redirectTerm: suggestion, term: suggestion }, () => this.search());
-  }
-
-  getSongsView() {
-    const { songs } = this.state;
-    if (songs.length === 0) {
-      return null;
-    }
-    return (
-      <div className={styles.section}>
-        <span className={styles.title}>Songs</span>
-        {
-          this.state.songs.map((song, index) => (
-            <Song
-              song={song}
-              onSelected={() => this.props.onSongSelected(this.state.songs, index)}
-            />
-          ))
-        }
+  return (
+    <div className={styles.section}>
+      <span className={styles.title}>Playlists</span>
+      <div className={styles.playlists}>
+        {playlists.map((playlist) => (
+          <Link
+            href={`/playlist/${Buffer.from(playlist.id).toString('base64')}`}
+            to={`/playlist/${Buffer.from(playlist.id).toString('base64')}`}
+          >
+            <Playlist key={playlist.id} playlist={playlist} />
+          </Link>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  getAlbumsView() {
-    const { albums } = this.state;
-    if (albums.length === 0) {
-      return null;
-    }
-    return (
-      <div className={styles.section}>
-        <span className={styles.title}>Albums</span>
-        <div className={styles.albums}>
-          {
-            this.state.albums.map((album) => (
-              <Album album={album} onSelected={this.props.onAlbumSelected} />
-            ))
-          }
-        </div>
+function ArtistsView({ artists }) {
+  if (artists.length === 0) {
+    return null;
+  }
+  return (
+    <div className={styles.section}>
+      <span className={styles.title}>Artists</span>
+      <div className={styles.artists}>
+        {artists.map((artist) => (
+          <Link
+            href={`/artist/${Buffer.from(artist.id).toString('base64')}`}
+            to={`/artist/${Buffer.from(artist.id).toString('base64')}`}
+          >
+            <Artist key={artist.id} artist={artist} artwork />
+          </Link>
+        ))}
       </div>
-    );
+    </div>
+  );
+}
+
+function SearchCatalog({ location, onAlbumSelected, onSongSelected }) {
+  const query = new URLSearchParams(location.search);
+
+  const [input, setInput] = useState('');
+  const [term, setTerm] = useState(query.get('term') || '');
+  const [isSearching, setIsSearching] = useState(term !== null);
+  const [songs, setSongs] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const [redirectTerm, setRedirectTerm] = useState(null);
+
+  function onSuggestionSelected(event, { suggestion }) {
+    setRedirectTerm(suggestion);
+    setTerm(suggestion);
   }
 
-  getArtistsView() {
-    const { artists } = this.state;
-    if (artists.length === 0) {
-      return null;
+  useEffect(
+    debounce(() => {
+      async function fetchSuggestions() {
+        const hints = await window.MusicKitInstance.api.searchHints(input);
+        setSuggestions(hints.terms);
+      }
+      fetchSuggestions();
+    }, 300),
+    [input],
+  );
+
+  useEffect(() => {
+    async function search() {
+      setIsSearching(true);
+
+      const {
+        songs: s,
+        albums: ab,
+        artists: at,
+        playlists: p,
+      } = await window.MusicKitInstance.api.search(term);
+
+      setIsSearching(false);
+      setSongs(get(s, 'data', []));
+      setAlbums(get(ab, 'data', []));
+      setArtists(get(at, 'data', []));
+      setPlaylists(get(playlists, 'data', []));
     }
-    return (
-      <div className={styles.section}>
-        <span className={styles.title}>Artists</span>
-        <div className={styles.artists}>
-          {
-            this.state.artists.map((artist) => (
-              <Link href={`/artist/${Buffer.from(artist.id).toString('base64')}`} to={`/artist/${Buffer.from(artist.id).toString('base64')}`}>
-                <Artist key={artist.id} artist={artist} artwork />
-              </Link>
-            ))
-          }
-        </div>
-      </div>
-    );
-  }
+    search();
+  }, [term]);
 
-  getPlaylistsView() {
-    const { playlists } = this.state;
-    if (playlists.length === 0) {
-      return null;
-    }
-    return (
-      <div className={styles.section}>
-        <span className={styles.title}>Playlists</span>
-        <div className={styles.playlists}>
-          {
-            this.state.playlists.map((playlist) => (
-              <Link href={`/playlist/${Buffer.from(playlist.id).toString('base64')}`} to={`/playlist/${Buffer.from(playlist.id).toString('base64')}`}>
-                <Playlist playlist={playlist} />
-              </Link>
-            ))
-          }
-        </div>
-      </div>
-    );
-  }
-
-  getResultsView() {
-    if (this.state.isSearching) {
-      return (
-        <Loader />
-      );
-    }
-    return (
-      <div className={styles.results}>
-        { this.getArtistsView() }
-        { this.getSongsView() }
-        { this.getAlbumsView() }
-        { this.getPlaylistsView() }
-      </div>
-    );
-  }
-
-  async fetchSuggestions({ value }) {
-    const hints = await window.MusicKitInstance.api.searchHints(value);
-    this.setState({ suggestions: hints.terms });
-  }
-
-  async clearSuggestions() {
-    this.setState({ suggestions: [] });
-  }
-
-  async search() {
-    this.setState({ isSearching: true });
-
-    const {
-      songs,
-      albums,
-      artists,
-      playlists,
-    } = await window.MusicKitInstance.api.search(this.state.term);
-
-    this.setState({
-      isSearching: false,
-      songs: songs.data,
-      albums: albums.data,
-      artists: artists.data,
-      playlists: playlists ? playlists.data : [],
-    });
-  }
-
-  render() {
-    const { redirectTerm } = this.state;
-    return (
-      <div className={styles.container}>
-        {
-          redirectTerm ? <Redirect to={`/search?term=${redirectTerm}`} /> : null
-        }
-        <form
-          className={styles.search}
-          onSubmit={(e) => {
+  return (
+    <div className={styles.container}>
+      {redirectTerm ? <Redirect to={`/search?term=${redirectTerm}`} /> : null}
+      <form
+        className={styles.search}
+        onSubmit={(e) => {
           e.preventDefault();
-          this.setState({ redirectTerm: this.state.term });
-          this.search();
+          setRedirectTerm(term);
         }}
-        >
-          <Autosuggest
-            suggestions={this.state.suggestions}
-            onSuggestionsFetchRequested={this.fetchSuggestions}
-            onSuggestionsClearRequested={this.clearSuggestions}
-            onSuggestionSelected={this.onSuggestionSelected}
-            renderSuggestion={this.getSuggestionView}
-            getSuggestionValue={(sug) => sug}
-            inputProps={{
-              value: this.state.term,
-              onChange: this.onChange,
-            }}
-            theme={{
-              container: styles.input,
-              suggestionsList: styles['suggestions-list'],
-            }}
-          />
-          <input type="submit" value="Search" />
-        </form>
-        { this.getResultsView() }
-      </div>
-    );
-  }
+      >
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={() => {}}
+          onSuggestionsClearRequested={() => setSuggestions([])}
+          onSuggestionSelected={onSuggestionSelected}
+          renderSuggestion={(text) => (
+            <div className={styles.suggestion}>
+              <span>{text}</span>
+            </div>
+          )}
+          getSuggestionValue={(sug) => sug}
+          inputProps={{
+            value: input,
+            onChange: (_, { newValue }) => setInput(newValue),
+          }}
+          theme={{
+            container: styles.input,
+            suggestionsList: styles['suggestions-list'],
+          }}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      {isSearching && <Loader />}
+      {!isSearching && (
+        <div className={styles.results}>
+          <ArtistsView artists={artists} />
+          <SongsView songs={songs} onSongSelected={onSongSelected} />
+          <AlbumsView albums={albums} onAlbumSelected={onAlbumSelected} />
+          <PlaylistsView playlists={playlists} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 SearchCatalog.propTypes = {
