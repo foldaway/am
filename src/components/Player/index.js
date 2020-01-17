f0967123a585f4e55725a60ed0596e64ec525fe0
Duncan Leo <hello@duncanleo.me>
2020-01-17 23:51:59 +0800

    chore: make Player a functional component

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Song from '../Song';
@@ -8,65 +8,54 @@ import trackPropType from '../../prop_types/track';
 
 import styles from './styles.scss';
 
-class Player extends Component {
-  constructor(props) {
-    super(props);
+function Player(props) {
+  const { queue, nowPlayingItemIndex, playbackState } = props;
+  const activeRef = useRef(null);
 
-    this.activeRef = null;
-    this.activeRefFunc = (e) => {
-      this.activeRef = e;
-    };
-  }
-
-  componentDidUpdate() {
-    if (this.activeRef !== null) {
-      this.activeRef.scrollIntoView({ behaviour: 'smooth' });
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current.scrollIntoView({ behaviour: 'smooth' });
     }
-    console.log(window.MusicKit.PlaybackStates[this.props.playbackState]);
-  }
+  }, [activeRef]);
 
-  render() {
-    const { queue, nowPlayingItemIndex, playbackState } = this.props;
+  const {
+    MusicKitInstance: { player },
+  } = window;
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.queue}>
-          <span className={styles.title}>Queue</span>
-          {
-            queue.items.map((item, index) => (
-              <div
-                key={item.id}
-                className={nowPlayingItemIndex === index ? styles.active : null}
-                ref={nowPlayingItemIndex === index ? this.activeRefFunc : null}
-              >
-                <Song
-                  song={item}
-                  onSelected={() => window.MusicKitInstance.player.changeToMediaAtIndex(index)}
-                />
-              </div>
-            ))
-          }
-        </div>
-        <div className={styles.controls}>
-          <PlayerControls
-            hasPrevious={queue.position > 0}
-            hasNext={queue.position < queue.length - 1}
-            onSeek={(time) => window.MusicKitInstance.player.seekToTime(time)}
-            onPrevious={() => window.MusicKitInstance.player.changeToMediaAtIndex(queue.position - 1)}
-            onNext={() => window.MusicKitInstance.player.skipToNextItem()}
-            onPlaybackChange={(isPaused) => (isPaused ?
-              window.MusicKitInstance.player.play() :
-              window.MusicKitInstance.player.pause()
-            )}
-            onVolumeChange={(vol) => { window.MusicKitInstance.player.volume = vol; }}
-            playbackState={playbackState}
-          />
-        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.queue}>
+        <span className={styles.title}>Queue</span>
+        {queue.items.map((item, index) => (
+          <div
+            key={item.id}
+            className={nowPlayingItemIndex === index ? styles.active : null}
+            ref={nowPlayingItemIndex === index ? activeRef : null}
+          >
+            <Song
+              song={item}
+              onSelected={() => player.changeToMediaAtIndex(index)}
+            />
+          </div>
+        ))}
       </div>
-    );
-  }
+      <div className={styles.controls}>
+        <PlayerControls
+          hasPrevious={queue.position > 0}
+          hasNext={queue.position < queue.length - 1}
+          onSeek={(time) => player.seekToTime(time)}
+          onPrevious={() => player.changeToMediaAtIndex(queue.position - 1)}
+          onNext={() => player.skipToNextItem()}
+          onPlaybackChange={(isPaused) => (isPaused ? player.play() : player.pause())}
+          onVolumeChange={(vol) => {
+            player.volume = vol;
+          }}
+          playbackState={playbackState}
+        />
+      </div>
+    </div>
+  );
 }
-
 Player.defaultProps = {
   nowPlayingItemIndex: 0,
 };
@@ -75,6 +64,7 @@ Player.propTypes = {
   queue: PropTypes.shape({
     items: PropTypes.arrayOf(trackPropType),
     position: PropTypes.number,
+    length: PropTypes.number,
   }).isRequired,
   nowPlayingItemIndex: PropTypes.number,
   playbackState: PropTypes.number.isRequired,
