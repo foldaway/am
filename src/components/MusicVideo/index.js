1092da8c601d7f447af57051b38f9d1ec4b5ee44
Duncan Leo <hello@duncanleo.me>
2020-01-19 21:38:48 +0800

    chore: migrate MusicVideo to styled-components

--- src/components/MusicVideo/index.js
+++ src/components/MusicVideo/index.js
@@ -1,23 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './styles.scss';
+import styled from 'styled-components';
 
 import { imgURLGen, srcSetGen } from '../../util/img';
 
 import musicVideoPropType from '../../prop_types/musicVideo';
 
+const Art = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  grid-area: art;
+  border-radius: 3px;
+  transition: filter 200ms;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr auto auto;
+  grid-template-areas:
+    "art"
+    "title"
+    "year";
+  grid-row-gap: 4px;
+
+  &:hover {
+    cursor: pointer;
+
+    ${Art} {
+      filter: brightness(120%);
+    }
+  }
+`;
+
+const Title = styled.span`
+  grid-area: title;
+  font-size: 0.8em;
+  color: ${(props) => props.theme.text.primary};
+`;
+
+const Year = styled.span`
+  grid-area: year;
+  font-size: 0.75em;
+  font-weight: 300;
+  color: $gray;
+  color: ${(props) => props.theme.text.secondary};
+`;
+
 const MusicVideo = ({ onSelected, musicVideo }) => (
-  <div className={styles.container} onClick={() => onSelected(musicVideo)} role="presentation">
-    <img
-      className={styles.art}
-      src={('artwork' in musicVideo.attributes) ? imgURLGen(musicVideo.attributes.artwork.url, { w: 75 }) : null}
-      srcSet={('artwork' in musicVideo.attributes) ? srcSetGen(musicVideo.attributes.artwork.url) : null}
+  <Wrapper onClick={() => onSelected(musicVideo)} role="presentation">
+    <Art
+      src={
+        'artwork' in musicVideo.attributes
+          ? imgURLGen(musicVideo.attributes.artwork.url, { w: 75 })
+          : null
+      }
+      srcSet={
+        'artwork' in musicVideo.attributes
+          ? srcSetGen(musicVideo.attributes.artwork.url)
+          : null
+      }
       alt="Song artwork"
     />
-    <span className={styles.title}>{musicVideo.attributes.name}</span>
-    <span className={styles.year}>{new Date(musicVideo.attributes.releaseDate).getFullYear()}</span>
-  </div>
+    <Title>{musicVideo.attributes.name}</Title>
+    <Year>{new Date(musicVideo.attributes.releaseDate).getFullYear()}</Year>
+  </Wrapper>
 );
 
 MusicVideo.defaultProps = {
